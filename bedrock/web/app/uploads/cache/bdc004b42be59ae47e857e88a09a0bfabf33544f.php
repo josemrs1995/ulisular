b5c2e1c24460e959eadb249d1c6f13b6eb59da51
josemrs1995 <josemrs1995@gmail.com>
2018-08-29 16:43:09 -0400

    tercer commit

---
+++ bedrock/web/app/uploads/cache/bdc004b42be59ae47e857e88a09a0bfabf33544f.php
@@ -0,0 +1,11 @@
+<header class="banner">
+  <div class="container">
+    
+    <nav class="nav-primary">
+      <?php if(has_nav_menu('primary_navigation')): ?>
+        <?php echo wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']); ?>
+
+      <?php endif; ?>
+    </nav>
+  </div>
+</header>
